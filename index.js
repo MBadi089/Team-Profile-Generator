6a2b26b5f84cbdf882c991e4f647840b0c753265
Marcos <marcosbadillo089@gmail.com>
2021-05-28 22:31:32 -0500

    added files for each job title in lib

---
+++ index.js
@@ -0,0 +1,14 @@
+const inquirer = require('inquirer');
+
+inquirer
+
+const questions = [
+    //ask the user for the employees name
+    {
+
+    },
+    //ask the user for the employee's job title (maybe use a mutiple choice and create an if else method to branch off for certain choices)
+    {
+
+    },
+]
