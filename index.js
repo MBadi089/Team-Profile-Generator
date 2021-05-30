6bea043648279e9444b58fb0a513ed6c71934f52
Marcos <marcosbadillo089@gmail.com>
2021-05-30 14:24:18 -0500

    deleted old questions and created new one

--- index.js
+++ index.js
@@ -1,115 +1,120 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const questions;
 
-inquirer
-//Ask the user to answer the questions for the employees name, role, etc.
-const questions = [
+const teamMembersArrary = [];
+
+//=============================================================================//
+//Ask the user to build their team starting with the manager.
+
+const managerQuestions = [
+    //ask for the manager's name
     {
-   //ask the user for the employees name 
-        type: 'userInput',
+        type: 'input',
         name: 'name',
-        message: 'Enter employees name',
-        validate: name => {
-            if (name) {
-                return true;
-            }
-            else {
-                console.log('Please enter the employees name!');
-                return false;
-            }
-        }
+        message: 'What is your Managers name?'
+    },
+    //ask for the manager's Id
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your Managers ID?',
+    },
+    //ask for the manager's email address
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the Managers email address?',
+    },
+    //ask for the managers office number
+    {
+        type: 'input',
+        name: 'office',
+        message: 'What is the Managers office number?',
+    },
+];
+//=============================================================================//
+
+//=============================================================================//
+//Ask the user to add members to their team with roles
+
+const teamMemberRole = {
+    type: 'input',
+    name: 'role',
+    message: 'What is the role of your team member? (Engineer or Intern)',
+    choices: ['Engineer', 'Intern'],
+};
+//=============================================================================//
+
+//=============================================================================//
+//Ask the user questions for the Engineering role
+
+const engineerQuestions = [
+    //ask for the engineer's name
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the Engineers name?',
     },
 
+    //ask for the engineer's ID
     {
-   //ask the user for the employee's job title (maybe use a mutiple choice and create an if else method to branch off for certain choices)     
-        type: 'list',
-        name: 'employeeRole',
-        message: 'Select employees role',
-        choices: ['Engineer', 'Intern' ,'Manager'],
-        validate: employeeRole => {
-            if (employeeRole == 'Engineer') {
-        {
-        //ask the user for the employee's id
-            type: 'userInput',
-            name: 'id',
-            message: 'Enter Engineers ID',
-        
-        },
+        type: 'input',
+        name: 'id',
+        message: 'What is the Engineers ID?',
+    },
+    
+    //ask for the engineers email address
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the Engineers email address?',
+    },
 
-        {
-        //ask the user for the employee's email address
-            type: 'userInput',
-            name: 'email',
-            message: 'Enter Engineers email address.',
-        },
-        //ask the user for the employee's github name
-        {
-            type: 'userInput',
-            name: 'github',
-            message: 'Enter Engineers github user name.'
-        }
-    
-    }
-    else if (employeeRole == 'Manager') {
-        {
-            //ask the user for the employee's id
-                type: 'userInput',
-                name: 'id',
-                message: 'Enter managers ID',
-            
-        },
-    
-        {
-            //ask the user for the employee's email address
-                type: 'userInput',
-                name: 'email',
-                message: 'Enter managers email address.',   
-        },
+    //ask for the engineers github user name
 
-        //ask the user for the manager's office number
-        {
-            type: 'userInput',
-            name: 'office',
-            message: 'Enter managers office number.',
-        },
-    }
-    else (employeeRole == 'Intern') {
-        {
-            //ask the user for the employee's id
-                type: 'userInput',
-                name: 'id',
-                message: 'Enter Interns ID',
-            
-        },
-    
-        {
-            //ask the user for the employee's email address
-                type: 'userInput',
-                name: 'email',
-                message: 'Enter Interns email address.',   
-        },
-        
-        {
-            //ask the user for the intern's school 
-            type: 'userInput',
-            name: 'education',
-            message: 'Enter Interns attending school.',
-        }
-    }
-
-    
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is the Engineers Github user name?',
+    },
 ];
+//=============================================================================//
 
-//TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(quesitons)
-    .then(function(userInput) {
-        console.log(userInput)
-    });
-};
+//=============================================================================//
+//Ask the user questions for the Intern role
 
-init();
+const internQuestions = [
+  //ask for the intern's name
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the Interns name?',
+    },
+
+    //ask for the intern's ID
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the Interns ID?',
+    },
+
+    //ask for the intern's email address
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the Interns email address?',
+    },
+    
+    //ask where the intern is attending school
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What school is the Intern attending?',
+    },
+];
