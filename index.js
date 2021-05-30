813ed1aae70d7eb96c517e894706284ea336bb86
Marcos <marcosbadillo089@gmail.com>
2021-05-30 13:50:11 -0500

    updated questions in index.js with if else but has errors

--- index.js
+++ index.js
@@ -31,30 +31,77 @@ const questions = [
         message: 'Select employees role',
         choices: ['Engineer', 'Intern' ,'Manager'],
         validate: employeeRole => {
-            if (employeeRole) {
-                return true;
-            }
-            else {
-                console.log('Please enter employees role!');
-                return false;
-            }
-        }
-    },
-
-    {
-    //ask the user for the employee's id
-        type: 'userInput',
-        name: 'id',
-        message: 'Enter employees ID',
+            if (employeeRole == 'Engineer') {
+        {
+        //ask the user for the employee's id
+            type: 'userInput',
+            name: 'id',
+            message: 'Enter Engineers ID',
         
-    },
+        },
 
-    {
-    //ask the user for the employee's email address
-        type: 'userInput',
-        name: 'email',
-        message: 'Enter employees email address',
-    },
+        {
+        //ask the user for the employee's email address
+            type: 'userInput',
+            name: 'email',
+            message: 'Enter Engineers email address.',
+        },
+        //ask the user for the employee's github name
+        {
+            type: 'userInput',
+            name: 'github',
+            message: 'Enter Engineers github user name.'
+        }
+    
+    }
+    else if (employeeRole == 'Manager') {
+        {
+            //ask the user for the employee's id
+                type: 'userInput',
+                name: 'id',
+                message: 'Enter managers ID',
+            
+        },
+    
+        {
+            //ask the user for the employee's email address
+                type: 'userInput',
+                name: 'email',
+                message: 'Enter managers email address.',   
+        },
+
+        //ask the user for the manager's office number
+        {
+            type: 'userInput',
+            name: 'office',
+            message: 'Enter managers office number.',
+        },
+    }
+    else (employeeRole == 'Intern') {
+        {
+            //ask the user for the employee's id
+                type: 'userInput',
+                name: 'id',
+                message: 'Enter Interns ID',
+            
+        },
+    
+        {
+            //ask the user for the employee's email address
+                type: 'userInput',
+                name: 'email',
+                message: 'Enter Interns email address.',   
+        },
+        
+        {
+            //ask the user for the intern's school 
+            type: 'userInput',
+            name: 'education',
+            message: 'Enter Interns attending school.',
+        }
+    }
+
+    
 ];
 
 //TODO: Create a function to initialize app
